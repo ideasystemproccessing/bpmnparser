1f9c625c0b1048c1d70b9a19df181857d4363c18
mehdi <mehdi.shokohifar@gmail.com>
2020-11-29 00:23:14 +0330

    Clean main.go File , Remove redundant Code

--- main.go
+++ main.go
@@ -6,38 +6,38 @@ import (
 	"strings"
 	"xmlParser/bpmn_parser"
 )
-func ParseToJson(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,proc *ConditionFlowElement){
-	var node string
+
+var Errors []string
+var Paths []string
+
+func ParseToJson(els []*bpmn_parser.Element, bpmn *bpmn_parser.Bpmn, proc *ConditionFlowElement) {
 	//println(len(els))
 
-	for _,el:=range els {
-		new_proc:=new(ConditionFlowElement)
-		param:=new(BpmnRuleCondition)
+	for _, el := range els {
+		new_proc := new(ConditionFlowElement)
+		param := new(BpmnRuleCondition)
 		switch el.GetType() {
 		case "Gateway":
-			node=el.GetElement().(*bpmn_parser.ExclusiveGateway).ID
-			if el.Element.(*bpmn_parser.ExclusiveGateway).RuleCondition!="" {
+			if el.Element.(*bpmn_parser.ExclusiveGateway).RuleCondition != "" {
 				err := jsoniter.Unmarshal([]byte(el.Element.(*bpmn_parser.ExclusiveGateway).RuleCondition), param)
 				if err != nil {
 					panic(err)
 				}
-			}else {
-				param=nil
+			} else {
+				param = nil
 			}
 		case "Activity":
-			node= el.GetElement().(*bpmn_parser.Task).ID
-			if el.Element.(*bpmn_parser.Task).RuleCondition!="" {
+			if el.Element.(*bpmn_parser.Task).RuleCondition != "" {
 
 				err := jsoniter.Unmarshal([]byte(el.Element.(*bpmn_parser.Task).RuleCondition), param)
 				if err != nil {
 					panic(err)
 				}
-			}else {
-				param=nil
+			} else {
+				param = nil
 			}
 		case "Event":
-			if endEvent,ok:=el.GetElement().(*bpmn_parser.EndEvent);ok {
-				node = endEvent.ID
+			if endEvent, ok := el.GetElement().(*bpmn_parser.EndEvent); ok {
 				if el.Element.(*bpmn_parser.EndEvent).RuleCondition != "" {
 					err := jsoniter.Unmarshal([]byte(endEvent.RuleCondition), param)
 					if err != nil {
@@ -46,8 +46,7 @@ func ParseToJson(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,proc *Conditi
 				} else {
 					param = nil
 				}
-			}else if interEvent,ok:=el.GetElement().(*bpmn_parser.IntermediateEvent);ok{
-				node = interEvent.ID
+			} else if interEvent, ok := el.GetElement().(*bpmn_parser.IntermediateEvent); ok {
 				if el.Element.(*bpmn_parser.IntermediateEvent).RuleCondition != "" {
 					err := jsoniter.Unmarshal([]byte(interEvent.RuleCondition), param)
 					if err != nil {
@@ -59,88 +58,89 @@ func ParseToJson(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,proc *Conditi
 			}
 		}
 		//println(node)
-		//println(el.PrevState.TestStatus)
-		new_proc.ConditionParams=param
-		new_proc.ConditionType=el.ElemId
-		new_proc.ElementType=el.GetElemType()
-		if el.PrevState.Name=="Y"{
+		new_proc.ConditionParams = param
+		new_proc.ConditionType = el.ElemId
+		new_proc.ElementType = el.GetElemType()
+		if el.PrevState.Name == "Y" {
 			proc.TrueState = new_proc
-		}else if el.PrevState.Name =="N" {
+		} else if el.PrevState.Name == "N" {
 			proc.FalseState = new_proc
 
-		}else {
-			proc.Next=append(proc.Next,new_proc)
+		} else {
+			proc.Next = append(proc.Next, new_proc)
 		}
 
-		getFirstStep:=bpmn.ForwardElement(node)
-		ParseToJson(getFirstStep,bpmn,new_proc)
+		getFirstStep := bpmn.ForwardElement(el.ElemId)
+		ParseToJson(getFirstStep, bpmn, new_proc)
 	}
 }
-var Errors []string
-var Paths []string
+
 func main() {
-	// Open our xmlFile
+	// BPMN Load File
 	err, bpmn := bpmn_parser.NewBPMN("export.bpmn")
 	if err != nil {
 		panic(err)
 	}
-	//getFirstStep := bpmn.Start()
-	getFirstStep:=bpmn.ForwardElement(bpmn.GetStartElement().ID)
-	proc:=new(ConditionFlowElement)
-	proc.ConditionType=bpmn.GetStartElement().ID
-	proc.ElementType= bpmn_parser.START_EVENT
-	ParseToJson(getFirstStep, bpmn,proc)
 
+	//====================================================
+	// Parse To Json Struct
+	getFirstStep := bpmn.ForwardElement(bpmn.GetStartElement().ID)
+	proc := new(ConditionFlowElement)
+	proc.ConditionType = bpmn.GetStartElement().ID
+	proc.ElementType = bpmn_parser.START_EVENT
+	ParseToJson(getFirstStep, bpmn, proc)
 
-	v,err:=jsoniter.Marshal(proc)
-	if err!=nil{
+	v, err := jsoniter.Marshal(proc)
+	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(string(v))
-	if len(bpmn.GetStartElement().Outgoing)==0{
-		Errors=append(Errors,"StartPoint Connection is required")
+
+	//====================================================
+	// Element Validation
+	if len(bpmn.GetStartElement().Outgoing) == 0 {
+		Errors = append(Errors, "StartPoint Connection is required")
 
 	}
-	if len(bpmn.GetStartElement().Outgoing)>1{
-		Errors=append(Errors,"The starting point can only have one connection ")
+	if len(bpmn.GetStartElement().Outgoing) > 1 {
+		Errors = append(Errors, "The starting point can only have one connection ")
 
 	}
-	DiagValidate(getFirstStep, bpmn,proc)
-	for _,err:=range Errors{
+	DiagValidate(getFirstStep, bpmn, proc)
+	for _, err := range Errors {
 		println(err)
 	}
 
-	var path  string
-	path=bpmn.GetStartElement().ID+" , "
-	SuccessEndValidation(getFirstStep,bpmn,&path)
+	//====================================================
+	//Path Validations
+	var path string
+	path = bpmn.GetStartElement().ID + " , "
+	SuccessEndValidation(getFirstStep, bpmn, &path)
 
-	for _,s:=range Paths{
-		if strings.Contains(s,"End") || strings.Contains(s,"Out_Of_Com") {
+	for _, s := range Paths {
+		if strings.Contains(s, "End") {
 			fmt.Println(s)
 		}
 	}
 }
 
+func DiagValidate(els []*bpmn_parser.Element, bpmn *bpmn_parser.Bpmn, proc *ConditionFlowElement) {
 
-func DiagValidate(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,proc *ConditionFlowElement){
-	var node string
-
-	for _,el:=range els {
-		if el.PrevState!=nil {
-			if strings.Split(el.PrevState.SourceRef,"_")[0]=="Gateway"{
-				if el.PrevState.Name!="Y" && el.PrevState.Name!="N"{
-					Errors=append(Errors,"The names of the Arrows must be `N` or `Y` : "+el.PrevState.Name)
+	for _, el := range els {
+		if el.PrevState != nil {
+			if strings.Split(el.PrevState.SourceRef, "_")[0] == "Gateway" {
+				if el.PrevState.Name != "Y" && el.PrevState.Name != "N" {
+					Errors = append(Errors, "The names of the Arrows must be `N` or `Y` : "+el.PrevState.Name)
 				}
 			}
 		}
-		new_proc:=new(ConditionFlowElement)
-		param:=new(BpmnRuleCondition)
+		new_proc := new(ConditionFlowElement)
+		param := new(BpmnRuleCondition)
 		switch el.GetType() {
 		case "Gateway":
-			node=el.GetElement().(*bpmn_parser.ExclusiveGateway).ID
-			if len(el.GetOutGoings())>2{
-				Errors=append(Errors,"This element should not have more than two connections : "+node)
+			if len(el.GetOutGoings()) > 2 {
+				Errors = append(Errors, "This element should not have more than two connections : "+el.ElemId)
 			}
 			//for _,selection_target:=range el.GetOutGoings(){
 			//
@@ -156,81 +156,52 @@ func DiagValidate(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,proc *Condit
 			//	}
 			//}
 		case "Activity":
-			node= el.GetElement().(*bpmn_parser.Task).ID
 			//if len(el.GetOutGoings())==1{
 			//	typeOfNext:=strings.Split(el.GetOutGoings()[0],"_")[0]
 			//	if typeOfNext=="Activity" {
 			//		Errors = append(Errors, "This element can only have Gateway or `Terminate Event` connections : "+node)
 			//	}
 			//}else
-			if len(el.GetOutGoings())>1{
-				Errors=append(Errors,"The minimum number of Task  is one Connection : "+node)
+			if len(el.GetOutGoings()) > 1 {
+				Errors = append(Errors, "The minimum number of Task  is one Connection : "+el.ElemId)
 
-			}else if len(el.GetOutGoings())==0{
-				Errors = append(Errors, "In task Design , A connection is required : "+node)
+			} else if len(el.GetOutGoings()) == 0 {
+				Errors = append(Errors, "In task Design , A connection is required : "+el.ElemId)
 			}
-
 
 		case "Event":
-			if endEvent,ok:=el.GetElement().(*bpmn_parser.EndEvent);ok {
-
-				node = endEvent.ID
-			}else if interEvent,ok:=el.GetElement().(*bpmn_parser.IntermediateEvent);ok {
-				node = interEvent.ID
-
-			}
-
 
 
 		}
 
-		//println(el.PrevState.TestStatus)
-		new_proc.ConditionParams=param
-		new_proc.ConditionType=el.ElemId
-		new_proc.ElementType=el.GetElemType()
-		if el.PrevState.Name=="Y"{
+		new_proc.ConditionParams = param
+		new_proc.ConditionType = el.ElemId
+		new_proc.ElementType = el.GetElemType()
+		if el.PrevState.Name == "Y" {
 			proc.TrueState = new_proc
-		}else if el.PrevState.Name =="N" {
+		} else if el.PrevState.Name == "N" {
 			proc.FalseState = new_proc
 
-		}else {
-			proc.Next=append(proc.Next,new_proc)
+		} else {
+			proc.Next = append(proc.Next, new_proc)
 		}
 
-		getFirstStep:=bpmn.ForwardElement(node)
-		DiagValidate(getFirstStep,bpmn,new_proc)
+		getFirstStep := bpmn.ForwardElement(el.ElemId)
+		DiagValidate(getFirstStep, bpmn, new_proc)
 	}
 }
 
-func SuccessEndValidation(els []*bpmn_parser.Element,bpmn *bpmn_parser.Bpmn,path * string){
-	var node string
-	//println(len(els))
-	Paths=append(Paths,*path)
+func SuccessEndValidation(els []*bpmn_parser.Element, bpmn *bpmn_parser.Bpmn, path *string) {
 
-var newPath string
-	for _,el:=range els {
+	Paths = append(Paths, *path)
 
-		switch el.GetType() {
-		case "Gateway":
-			node=el.GetElement().(*bpmn_parser.ExclusiveGateway).ID
-		case "Activity":
-			node=el.GetElement().(*bpmn_parser.Task).ID
-		case "Event":
-			if endEvent,ok:=el.GetElement().(*bpmn_parser.EndEvent);ok {
-				node = endEvent.ID
-			}else if interEvent,ok:=el.GetElement().(*bpmn_parser.IntermediateEvent);ok {
-				node = interEvent.ID
-			}
+	var newPath string
+	for _, el := range els {
+		newPath = *path + el.ElemId + " , "
+		if el.GetElemType() == "End" {
+			newPath += el.GetElemType()
 		}
-
-
-		newPath = *path + node + " , "
-
-		if el.GetElemType()=="End" || el.GetElemType()=="Out_Of_Commitment" {
-			 newPath+= el.GetElemType()
-		}
-
-		getFirstStep:=bpmn.ForwardElement(node)
-		SuccessEndValidation(getFirstStep,bpmn,&newPath)
+		getFirstStep := bpmn.ForwardElement(el.ElemId)
+		SuccessEndValidation(getFirstStep, bpmn, &newPath)
 	}
 }
